3f9611787ca55fcb815e60e9249d0fcc314256e5
gaboe <gabrielecegi@gmail.com>
2018-04-14 12:00:50 +0200

    print de jong function output

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Container, Menu, } from "semantic-ui-react";
-
+import { Link } from "react-router-dom";
 type Props = { children: React.ReactNode };
 
 const Layout: React.SFC<Props> = (props: Props) => (
@@ -9,14 +9,18 @@ const Layout: React.SFC<Props> = (props: Props) => (
             <Container>
                 <Menu.Item as="a" header={true}>
                     MAI
-        </Menu.Item>
-                <Menu.Item as="a">Home</Menu.Item>
+                </Menu.Item>
+                <Menu.Item as="a">
+                    <Link to="/">
+                        Random Search
+                    </Link>
+                </Menu.Item>
             </Container>
         </Menu>
 
-        <Container text={true} style={{ marginTop: "7em" }}>
+        <div style={{ marginTop: "7em", marginLeft: "4em" }}>
             {props.children}
-        </Container>
+        </div>
 
     </div>
 );
--- src/components/random-search/RandomSearch.tsx
+++ src/components/random-search/RandomSearch.tsx
@@ -1,12 +1,88 @@
 import * as React from "react";
-import { Header } from "semantic-ui-react";
+import { Header, List, ListItem } from "semantic-ui-react";
+import { randomInt, random } from "mathjs";
+import { Row } from "react-grid-system";
+
+type DejongInput = {
+  id: number;
+  iterations: number;
+  xi: number;
+  costValue: number;
+};
+
+type RoundWinner = {
+  input: DejongInput;
+  roundID: number;
+};
+
+const evaluateDejongFunction = (iterations: number, x: number) => {
+  return Array.from({ length: iterations })
+    .map(_ => Math.pow(x, 2))
+    .reduce((a, b) => a + b);
+};
+
+// const getDejongOptimalInput = () => {
+//   const dejongInput: DejongInput = {
+//     id: -1,
+//     iterations: 100,
+//     xi: 0,
+//     costValue: dejongFunction(100, 0)
+//   };
+//   return dejongInput;
+// };
+
+const getDejongRoundWinner = () => {
+  const winner: DejongInput = Array
+    .from({ length: 1000 })
+    .map((u, i) => i)
+    .map(e => {
+      const iterations = randomInt(1, 100);
+      const randomX = random(-5, 5);
+      const costValue = evaluateDejongFunction(iterations, randomX);
+      return { id: e, iterations: iterations, xi: randomX, costValue: costValue };
+    })
+    .sort((a, b) => (a.costValue - b.costValue))
+  [0];
+  console.log(winner.costValue);
+  return winner;
+};
+
+const getDejongInput = () => {
+  const input: RoundWinner[] = Array
+    .from({ length: 30 })
+    .map((u, i) => i)
+    .map(e => {
+      const winner: RoundWinner = {
+        roundID: e,
+        input: getDejongRoundWinner(),
+      };
+      return winner;
+    })
+    .sort((a, b) => (a.input.costValue - b.input.costValue));
+
+  return input;
+};
 
 const RandomSearch: React.SFC = () =>
   (
     <>
-      <Header as="h1">Semantic UI React Fixed Template</Header>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>A text container is used for the main container, which is useful for single column layouts.</p>
+      <Row>
+        <Header as="h1">De Jong First Function</Header>
+      </Row>
+      <Row>
+        <List>
+          {getDejongInput()
+            .map(x => {
+              return (
+                <ListItem key={x.roundID}>
+                  {`${x.roundID}. round: ${x.input.iterations}, x(i): 
+                ${x.input.xi}, costValue: ${x.input.costValue.toFixed(10)}`}
+                </ListItem>
+              );
+            })}
+        </List>
+      </Row>
+
     </>
   );
 

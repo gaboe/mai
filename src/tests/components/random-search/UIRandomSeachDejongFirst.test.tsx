b5b8bbaed19760e87a05ce5f83d9699fdf5d4cb3
gaboe <gabrielecegi@gmail.com>
2018-04-15 09:06:41 +0200

    Fix Snapshot tests

--- src/tests/components/random-search/UIRandomSeachDejongFirst.test.tsx
+++ src/tests/components/random-search/UIRandomSeachDejongFirst.test.tsx
@@ -3,13 +3,16 @@ import { RandomSearchFirstDejong } from "./../../../components/random-search/Ran
 import { RandomSearchSecondDejong } from "./../../../components/random-search/RandomSearchSecondDejong";
 import { RandomSearchSchwefel } from "./../../../components/random-search/RandomSearchSchwefel";
 import * as renderer from "react-test-renderer";
-import {
-  getFirstDejongStats,
-  getSecondDejongStats,
-  getSchwefelStats,
-  Stat
-} from "../../../services/randomSearch/RandomSearchService";
-import { getRandomColor } from "../../../services/ColorService";
+import { Stat } from "../../../services/randomSearch/RandomSearchService";
+
+beforeEach(() => {
+  const color = require("../../../services/ColorService");
+  color.getRandomColor = jest.fn(() => "#16031f");
+});
+afterEach(() => {
+  jest.unmock("../../../services/randomSearch/RandomSearchService");
+  jest.unmock("../../../services/ColorService");
+});
 
 const getTestData = () => {
   const response: Stat = {
@@ -49,24 +52,27 @@ const getTestData = () => {
 };
 
 test("Render first dejong function", () => {
-  getFirstDejongStats = jest.fn(() => getTestData());
-  getRandomColor = jest.fn(() => "#16031f");
+  const randomSearchServiceMock = require("../../../services/randomSearch/RandomSearchService");
+  randomSearchServiceMock.getFirstDejongStats = jest.fn(() => getTestData());
+
   const component = renderer.create(<RandomSearchFirstDejong />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test("Render second dejong function", () => {
-  getSecondDejongStats = jest.fn(() => getTestData());
-  getRandomColor = jest.fn(() => "#16031f");
+  const randomSearchServiceMock = require("../../../services/randomSearch/RandomSearchService");
+  randomSearchServiceMock.getSecondDejongStats = jest.fn(() => getTestData());
+
   const component = renderer.create(<RandomSearchSecondDejong />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test("Render Schwefel", () => {
-  getSchwefelStats = jest.fn(() => getTestData());
-  getRandomColor = jest.fn(() => "#16031f");
+  const randomSearchServiceMock = require("../../../services/randomSearch/RandomSearchService");
+  randomSearchServiceMock.getSchwefelStats = jest.fn(() => getTestData());
+
   const component = renderer.create(<RandomSearchSchwefel />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
